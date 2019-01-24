3fd410dea9938834fe934622ced1a500e02a6738
Nikolay Slavov <nikolay@slavov.org>
2019-01-24 14:36:37 +0200

    Add headers support

--- src/ApolloClient.js
+++ src/ApolloClient.js
@@ -3,8 +3,16 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import fetch from 'node-fetch';
 
-export default (uri) => {
-  const httpLink = createHttpLink({ uri: uri, fetch });
+export default (uri, headers = {}) => {
+  // TODO: throw exception if uri is not set. Add test
+
+  let httpLinkOptions = { uri: uri, fetch }
+
+  if (Object.keys(headers).length > 0) {
+    httpLinkOptions.headers = headers;
+  }
+
+  const httpLink = createHttpLink(httpLinkOptions);
 
   let apolloParams = { cache: new InMemoryCache(), link: httpLink };
   return new ApolloClient(apolloParams);
